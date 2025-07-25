7b1c23885b6b78ce2290ab8b3fc969b1c37ece75
bsantosanisio@gmail.com <bsantosanisio@gmail.com>
2025-07-25 15:50:45 +0100

    feat: view function in portal added!

--- revengai/features/__init__.py
+++ revengai/features/__init__.py
@@ -4,5 +4,6 @@ from .auto_unstrip import AutoUnstripFeature
 from .choose_source import ChooseSourceFeature
 from .match_functions import MatchFunctionsFeature
 from .match_current_function import MatchCurrentFunctionFeature
+from .view_function_in_portal import ViewFunctionInPortalFeature
 
-__all__ = ['ConfigurationFeature', 'UploadFeature', 'AutoUnstripFeature', 'ChooseSourceFeature', 'MatchFunctionsFeature', 'MatchCurrentFunctionFeature'] 
+__all__ = ['ConfigurationFeature', 'UploadFeature', 'AutoUnstripFeature', 'ChooseSourceFeature', 'MatchFunctionsFeature', 'MatchCurrentFunctionFeature', 'ViewFunctionInPortalFeature'] 
